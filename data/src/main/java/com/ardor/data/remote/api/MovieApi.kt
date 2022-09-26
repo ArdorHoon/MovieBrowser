e864901c14fa8a752e3e5d6fccf343140416eb7b
ArdorHoon <didgnsah7320@gmail.com>
2022-09-26 13:34:06 +0900

    add keys

--- data/src/main/java/com/ardor/data/remote/api/MovieApi.kt
+++ data/src/main/java/com/ardor/data/remote/api/MovieApi.kt
@@ -1,12 +1,13 @@
 package com.ardor.data.remote.api
 
+import com.ardor.data.API_KEY
 import com.ardor.data.remote.model.Movie
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface MovieApi {
-    @GET("")
+    @GET("?i=tt3896198&apikey=${API_KEY}")
     suspend fun getMovies(
         @Query("s") s : String,
         @Query("type") type : String?,
