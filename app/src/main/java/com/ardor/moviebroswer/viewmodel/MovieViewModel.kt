31ab413fec96719c41fcf019b68c094b17dd2b8b
ArdorHoon <didgnsah7320@gmail.com>
2022-10-13 12:09:58 +0900

    apply stateFlow

--- app/src/main/java/com/ardor/moviebroswer/viewmodel/MovieViewModel.kt
+++ app/src/main/java/com/ardor/moviebroswer/viewmodel/MovieViewModel.kt
@@ -1,10 +1,12 @@
 package com.ardor.moviebroswer.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.viewModelScope
+import com.ardor.domain.model.SearchEntity
 import com.ardor.domain.usecase.GetMoviesUseCase
 import com.ardor.moviebroswer.core.base.BaseViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -13,9 +15,13 @@ class MovieViewModel @Inject constructor(
     private val getMoviesUseCase: GetMoviesUseCase
 ) : BaseViewModel() {
 
+    private val _searchResults : MutableStateFlow<List<SearchEntity>?> = MutableStateFlow(null)
+    val searchResult : StateFlow<List<SearchEntity>?> =  _searchResults
+
+
     fun getMovies(title: String) {
         viewModelScope.launch {
-            val temp = getMoviesUseCase(title)
+            _searchResults.value = getMoviesUseCase(title).search
         }
     }
 }
